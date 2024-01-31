19c86a731e4400fa33917bebbbc47b113c2305f6
Isaac Machado <isaac991mg@gmail.com>
2024-01-30 17:16:02 -0700

    Formulario ClasificacionExp

---
+++ SistProduccionEnMVC/Catalogos/frmCAT_ClasificacionExp.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmCAT_ClasificacionExp.aspx.cs" Inherits="SistProduccionEnMVC.Catalogos.frmCAT_ClasificacionExp" %>
+
+<%@ Register assembly="DevExpress.Web.v19.1, Version=19.1.3.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web" tagprefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body style="height: 541px; background-color: black">
+    <form id="form1" runat="server" style="text-align: right; height: 538px; width: 1149px; font-size: 36px;" submitdisabledcontrols="False">
+        <p style="text-align: center">
+            &nbsp;</p>
+        <p style="text-align: center">
+            &nbsp;</p>
+        <p style="text-align: center; font-size: 72px; color: #FFFFFF;">
+            Nombre:
+            <asp:TextBox ID="txt_nombre" runat="server" Font-Size="72pt" Height="77px" Width="607px"></asp:TextBox>
+        </p>
+        <p style="text-align: center">
+            &nbsp;</p>
+        <p style="text-align: center">
+            &nbsp;</p>
+        <p style="text-align: right; font-weight: 36; padding-right: 20px;">
+            <asp:Button ID="btn_nuevo" runat="server" BackColor="DeepSkyBlue" Font-Size="36pt" ForeColor="White" Height="137px" Text="Nuevo" Width="286px" />
+            <asp:Button ID="btn_guardar" runat="server" BackColor="LimeGreen" Font-Size="36pt" ForeColor="White" Height="137px" OnClick="Button2_Click" Text="Guardar" Width="286px" />
+            <asp:Button ID="btn_cancelar" runat="server" BackColor="Red" Font-Size="36pt" ForeColor="White" Height="137px" Text="Cancelar" Width="286px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ SistProduccionEnMVC/Catalogos/frmCAT_ClasificacionExp.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SistProduccionEnMVC.Catalogos
+{
+
+
+    public partial class frmCAT_ClasificacionExp
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+
+        /// <summary>
+        /// Control btn_nuevo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_nuevo;
+
+        /// <summary>
+        /// Control btn_guardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_guardar;
+
+        /// <summary>
+        /// Control btn_cancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_cancelar;
+    }
+}
